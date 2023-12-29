0ee2f0b475ab9b8317a4dfc28354260a02558e4c
win9 <krrr883@gmail.com>
2023-12-29 12:23:47 +0900

    Jsoup을 이용한 크롤링

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,25 @@
-package org.example;public class Main {
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            String url = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=105";
+            Document doc = Jsoup.connect(url).get();
+            Elements newsHeadlines = doc.select("#main_content > div > div._persist > div.section_headline > ul > li > div.sh_text > a");
+
+            // 제목
+            for (Element newsHeadline : newsHeadlines) {
+                System.out.println(newsHeadline.text());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
